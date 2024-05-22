ff62ee25a35750a830b369ec744e0efd5dac7215
srivani anandarajah <srivani.anand@outlook.fr>
2024-05-22 08:51:55 +0200

    wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+10.times do |i|
+  Article.create(title: "#{i}")
+end
